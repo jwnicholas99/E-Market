96643a3258c3f44eecfc3cf29cb14e2028247216
jwnicholas99 <jwnicholas99@gmail.com>
2019-06-13 00:41:37 -0400

    Created Product and Review models

--- app/models.py
+++ app/models.py
@@ -7,6 +7,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(120), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    products = db.relationship('Product', backref='seller', lazy='dynamic')
+    reviews = db.relationship('Review', backref='reviewer', lazy='dynamic')
 
     def __repr__(self):
         return "<User {}>".format(self.username)
@@ -20,3 +22,18 @@ class User(UserMixin, db.Model):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), index=True)
+    price = db.Column(db.Float, index=True)
+    stock = db.Column(db.Integer, index=True)
+    grading = db.Column(db.Integer, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    review = db.relationship('Review', backref='product', lazy='dynamic')
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    grading = db.Column(db.Integer)
+    comments = db.Column(db.String(1000))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
