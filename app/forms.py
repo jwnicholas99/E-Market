9e121f29e9c21f8eb18f28604fdd2d38e395ab4b
jwnicholas99 <jwnicholas99@gmail.com>
2019-06-12 23:27:22 -0400

    Built user model, migrated to database, built login and registration forms, built logout function

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,26 @@
-from flask_wtforms import FlaskForm
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired()
+from wtforms.validators import DataRequired, EqualTo, Email, ValidationError
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validors=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class RegistrationForm(FlaskForm):
+    username= StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Submit')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('This username has been taken already.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('This email has already been registered.')
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,48 @@
-from app import app
-from flask import render_template, url_for
+from app import app, db
+from flask import render_template, url_for, redirect, flash, request
+from flask_login import login_required, current_user, login_user, logout_user
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
+from werkzeug.urls import url_parse
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("The username or password is not correct")
+            return redirect(url_for('index'))
+        login_user(user)
+        next_url = request.args.get('next')
+        if not next_url or url_parse(next_url).netloc != '':
+            return redirect(url_for('index'))
+        return redirect(next_url)
+    return render_template('login.html', title='Sign-In', form=form)
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     return render_template('index.html', title="Home")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been registered!')
+        return redirect(url_for('login'))
+    return render_template('registration.html', title='Register', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
